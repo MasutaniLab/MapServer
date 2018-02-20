bd6c3a7171213134a2ccd5ea04dafd58846fe348
Keitinn <keitinn.reader@gmail.com>
2018-02-20 16:46:47 +0900

    起動バッチファイルにjyamlの設定を追加

--- MapServer.bat
+++ MapServer.bat
@@ -1,5 +1,5 @@
 @echo off
-set CLASSPATH=.\bin;%RTM_JAVA_ROOT%\jar\OpenRTM-aist-1.1.2.jar;%RTM_JAVA_ROOT%\jar\commons-cli-1.1.jar
+set CLASSPATH=.\bin;.\jar\jyaml-1.3.jar;%RTM_JAVA_ROOT%\jar\OpenRTM-aist-1.1.2.jar;%RTM_JAVA_ROOT%\jar\commons-cli-1.1.jar
 pushd  %~dp0
 start "%~n0" java %~n0Comp -f rtc.conf -o manager.os.hostname:%COMPUTERNAME% %*
 popd
